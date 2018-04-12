5606b61e82bcdf8fcbe3ecbfe034ab9df27d2bd9
Jeroen De Ryck <jeroenderyck1@gmail.com>
2018-04-12 17:40:17 +0200

    Create Quandl_futures.py

---
+++ Quandl_futures.py
@@ -0,0 +1,26 @@
+class QuandlFutures(object):
+
+     # This class deals with handling quandl futures
+     
+    def __init__(self):
+        #The config file will be a stored csv in the future, it holds all instruments we would like to get from quandl.
+        
+        self.config_data =pd.DataFrame({'CODE':'EDOLLAR','QCODE':'ED','FACTOR':1,'MARKET':'CME','FIRST_CONTRACT':198203},index=[0]).set_index('CODE')
+        
+    def get_list_of_instruments(self):
+        # this method returns all the instruments we can get
+        
+        return list(self.config_data.index)
+        
+    def get_instrument_config(self, instrument_code):
+    
+        # This method will try to get all the information on a specific instrument
+        if instrument_code not in self.get_list_of_instruments():
+            raise Exception("Instrument %s missing from config file " % (instrument_code))
+
+        data_for_code = self.config_data.loc[instrument_code]
+
+        return data_for_code
+        
+    def get_quandl_code_for_instrument(self,instrument):
+        return self.config_data.loc[instrument,'QCODE']
